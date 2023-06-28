3964ec720bf681ce908c9ab7d9f8460a3895a64c
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-06-28 17:09:04 +0200

    feat(api): add router and healthz

---
+++ api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/manifoldco/healthz"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	handler := healthz.NewHandler(r)
+	http.ListenAndServe(":3000", handler)
+}
