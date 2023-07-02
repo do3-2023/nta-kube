0892b56a7fedc647a5320d859ae0350c19bceeef
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-07-03 01:03:58 +0200

    feat(web): add correct /healthz endpoint

--- webapp/src/routes/healthz/+server.ts
+++ webapp/src/routes/healthz/+server.ts
@@ -1,6 +1,15 @@
-import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET = (() => {
-    return new Response();
-}) satisfies RequestHandler;
+export const GET: RequestHandler = async () => {
+  try {
+    const response = await fetch('http://api:3000');
+    if (response.ok) {
+      return new Response(null, { status: 200 });
+    } else {
+      return new Response(null, { status: 500 });
+    }
+  } catch (error) {
+    return new Response(null, { status: 500 });
+  }
+};
+
