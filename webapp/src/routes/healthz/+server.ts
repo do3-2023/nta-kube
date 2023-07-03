ac1facded0c048500ce710c6aa87089e91fe1da7
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-07-03 16:44:43 +0200

    fix(web): fix healthz route with correct url

--- webapp/src/routes/healthz/+server.ts
+++ webapp/src/routes/healthz/+server.ts
@@ -2,9 +2,9 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
   try {
-    const response = await fetch('http://api:3000');
+    const response = await fetch('http://nta-kube-api.api.svc.cluster.local:3000');
     if (response.ok) {
-      return new Response(null, { status: 200 });
+      return new Response("ok", { status: 200 });
     } else {
       return new Response(null, { status: 500 });
     }
