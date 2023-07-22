f72943f8885390f5c57d79f7beb7cd75d8a21f3d
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-07-22 19:09:51 +0200

    fix(web): handle null return from api

--- webapp/src/routes/+page.ts
+++ webapp/src/routes/+page.ts
@@ -2,6 +2,7 @@ import type { Drink } from '$lib/types';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-    let drinks: Drink[] = await fetch('/drinks').then(res => res.json());
+    let drinks: Drink[] | null = await fetch('/drinks').then(res => res.json());
+    if (drinks === null) drinks = [];
     return { drinks };
 }) satisfies PageLoad;
